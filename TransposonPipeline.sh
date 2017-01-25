1c907c85f8b3f67916986751ee8fbcc0f0484e40
rajewski <rajewski23@gmail.com>
2017-01-25 11:34:36 -0800

    test

--- TransposonPipeline.sh
+++ TransposonPipeline.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+#this script is based on methods from "The complex sequence landscape of maize revealed by single molecule technologies" by Jiao et al http://biorxiv.org/content/early/2016/12/19/079004
+#The GyDB database is used here (http://gydb.org/gydbModules/collection/collection/db/GyDB_collection.zip)
+ 
 module load genometools
+module load hmmer
 
-gt suffixerator -db scaffold584.fasta.gz -indexname scaffold584.fsa -tis -suf -lcp -des -ssp -sds -dna
-gt ltrharvest -index scaffold584.fsa -v -out pred-scaffold584.fsa -outinner pred-inner-scaffold584.fsa
+#use LTRHarvest to find transposons in the scaffold
+gt suffixerator -db $1.fasta.gz -indexname $1.fsa -tis -suf -lcp -des -ssp -sds -dna
+gt ltrharvest -index $1.fsa -v -out pred-$1.fsa -outinner pred-inner-$1.fsa -gff3 pred-$1.gff3
+
+#Use LTR digest to annotate them
+gt ltrdigest -force -hmms ~/shared/GyDB_collection/profiles/*.hmm -o anno_$1.gff3  pred-$1.gff3 $1.fsa
